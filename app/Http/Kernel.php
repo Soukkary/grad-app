87f7d501a2555b684542c27a173661a6ddae7644
Soukkary <youssefelsoukkary@gmail.com>
2024-06-21 20:18:06 +0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -49,7 +49,7 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Illuminate\Http\Middleware\HandleCors::class,
-            \App\Http\Middleware\Cors::class,
+            
             \Illuminate\Session\Middleware\StartSession::class, // Add this for session support
             \App\Http\Middleware\Cors::class, // Ensure CORS middleware is applied
         ],
